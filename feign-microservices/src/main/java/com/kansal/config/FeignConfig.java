ef04eba9871298cb5d472f3fa245f7fb7f326f89
kansology <tarun.kansal86@gmail.com>
2017-05-05 00:06:16 -0600

    add the zipkin publish functionality to microervices.

---
+++ feign-microservices/src/main/java/com/kansal/config/FeignConfig.java
@@ -0,0 +1,18 @@
+package com.kansal.config;
+
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by tarunkansal on 5/4/17.
+ */
+
+//@Configuration
+public class FeignConfig {
+
+   /* @Bean
+    public AlwaysSampler defaultSampler() {
+        return new AlwaysSampler();
+    }*/
+}
